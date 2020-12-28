8f9a8b13994eea9c0133ca16afd38c7b5eba4140
Valentina Perishko <valentina.perishko@gmail.com>
2020-12-27 23:51:52 -0600

    adds chsnges

---
+++ task2.js
@@ -0,0 +1,23 @@
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+  const toggleUserState = (allUsers, userName) => {
+    return new Promise((resolve) => {
+      const updatedUsers = allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      );
+      resolve(updatedUsers)
+      });
+  };
+  
+  const logger = updatedUsers => console.table(updatedUsers);
+  
+  toggleUserState(users, 'Mango').then(logger);
+  toggleUserState(users, 'Lux').then(logger);
+  
