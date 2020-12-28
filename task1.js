e37f3ab5f9447e9f86521fc078835f99cb6bbc48
Valentina Perishko <valentina.perishko@gmail.com>
2020-12-28 08:54:32 -0600

    adds destructuring

--- task1.js
+++ task1.js
@@ -1,22 +1,22 @@
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-    return new Promise((resolve, reject) => {
-      const passed = ms >= 0;
-      setTimeout(() => {
-        if(passed) {
-          resolve(ms);
-        } reject('error');
-      }, ms);
-    });
-  };
-  
-  const logger = time => console.log(`Resolved after ${time} ms`);
-  
-  
-  // Вызовы функции для проверки
-  delay(2000).then(logger); // Resolved after 2000ms
-  delay(1000).then(logger); // Resolved after 1000ms
-  delay(1500).then(logger); // Resolved after 1500ms
-  
-  
+  return new Promise((resolve, reject) => {
+    const passed = ms >= 0;
+    setTimeout(() => {
+      if (passed) {
+        resolve(ms);
+      }
+      else reject('error');
+    }, ms);
+  });
+};
+
+const logger = time => console.log(`Resolved after ${time} ms`);
+const mistake = error => console.log(error);
+
+// Вызовы функции для проверки
+delay(2000).then(logger).catch(mistake); // Resolved after 2000ms
+delay(1000).then(logger).catch(mistake); // Resolved after 1000ms
+delay(1500).then(logger).catch(mistake); // Resolved after 1500ms
+delay(-150).then(logger).catch(mistake);
